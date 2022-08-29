f86dd3e9e85f22d7c8d20ab544cea2bcf9070bec
asalihaj <as48243@ubt-uni.net>
2022-08-29 17:40:40 +0200

    first commit

---
+++ car/src/test/java/com/rental/car/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
