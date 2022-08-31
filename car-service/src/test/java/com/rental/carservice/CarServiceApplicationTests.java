c2212577df56fa18ca0c7179428901f6664e37c7
asalihaj <as48243@ubt-uni.net>
2022-09-01 00:22:04 +0200

    Added React project folders

---
+++ car-service/src/test/java/com/rental/carservice/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
