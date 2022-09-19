672cc286ff6e9e4dbbf96676d708963888395ff4
asalihaj <as48243@ubt-uni.net>
2022-09-19 21:02:47 +0200

    Added Security Service Project

---
+++ backend/security-jwt-service/src/test/java/com/rental/securityjwtservice/SecurityJwtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.securityjwtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
